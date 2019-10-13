1d79a6f2a4fb30d2644dd6b168a4eb0083863632
Maurício Junior <junior9771@gmail.com>
2019-10-13 13:04:29 -0300

    Singleton Pattern

---
+++ src/main/kotlin/creational/Singleton.kt
@@ -0,0 +1,18 @@
+package creational
+
+import java.util.concurrent.atomic.AtomicInteger
+
+object Singleton {
+    private val counter = AtomicInteger(0)
+    fun increment() = counter.incrementAndGet()
+
+    init {
+        println("I was accessed for the first time - $this")
+    }
+}
+
+fun main(){
+    for (i in 1..10) {
+        println("Singleton counter: ${Singleton.increment()} - ${Singleton.toString()}")
+    }
+}
